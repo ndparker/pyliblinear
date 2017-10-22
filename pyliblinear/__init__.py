d0962ba74dd9adc428a1a5f95d9588030932ba0a
ndparker <ndparker@users.noreply.github.com>
2017-10-22 15:18:41 +0200

    bump version to 211.dev1

--- pyliblinear/__init__.py
+++ pyliblinear/__init__.py
@@ -28,7 +28,7 @@ pyliblinear - a liblinear python API
 __author__ = u"Andr\xe9 Malo"
 __docformat__ = "restructuredtext en"
 __license__ = "Apache License, Version 2.0"
-__version__ = '2.1.0.dev2'
+__version__ = '211.dev1'
 
 from pyliblinear import _util
 from pyliblinear._liblinear import FeatureMatrix  # noqa
