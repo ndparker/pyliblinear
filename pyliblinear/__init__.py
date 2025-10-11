fcb92beff172645cbf0bae11fc82b3455f747734
ndparker <ndparker@users.noreply.github.com>
2025-10-11 23:45:15 +0200

    bump version to 249.1

--- pyliblinear/__init__.py
+++ pyliblinear/__init__.py
@@ -27,7 +27,7 @@ pyliblinear - a liblinear python API
 """
 __author__ = u"Andr\xe9 Malo"
 __license__ = "Apache License, Version 2.0"
-__version__ = "249.0"
+__version__ = "249.1"
 __all__ = ["FeatureMatrix", "Model", "Solver", "SOLVER_TYPES"]
 
 try:
